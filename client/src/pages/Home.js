7d21f4802f9013c34b64fa073ea7670f1d24fbd2
tarique4a <tarique.nitjsr77@gmail.com>
2022-05-29 14:06:22 +0530

    2nd commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import DefaultLayout from '../components/DefaultLayout'
+import React, { useEffect } from "react";
+import DefaultLayout from "../components/DefaultLayout";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCars } from "../redux/actions/carsActions";
 
 export default function Home() {
+  const { cars , loading} = useSelector((state) => state.carsReducer);
+  const dispatch = useDispatch();
+ 
+  useEffect(()=>{
+    dispatch(getAllCars())
+  },[])
+
   return (
     <DefaultLayout>
       <h1>Home page</h1>
+      <h1>{cars.length}</h1>
     </DefaultLayout>
-  )
+  );
 }
